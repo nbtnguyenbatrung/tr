adfa2982178d5b7b61b0feaee583c3b0ef7bbc2b
batrung <batrung255@gmail.com>
2021-11-26 17:03:20 +0700

    update\

--- miniblogs/src/component/Contact.js
+++ miniblogs/src/component/Contact.js
@@ -2,15 +2,15 @@ import React from "react";
 import axios from "axios";
 import Information from "./Infomation";
 import '../css/contact.scss';
-class Contact extends React.Component{
+class Contact extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             contact: {
-                id : "",
-                name : "",
-                email : "",
-                message : "",
+                id: "",
+                name: "",
+                email: "",
+                message: "",
             }
         }
     }
@@ -24,12 +24,13 @@ class Contact extends React.Component{
                 }
             })
             .then(res => {
+                console.log(res.data.id);
                 this.setState({
-                    new: {
-                        id : res.data.id,
-                        name : res.data.name ,
-                        email : res.data.email ,
-                        message : res.data.message ,
+                    contact: {
+                        id: res.data.id,
+                        name: res.data.name,
+                        email: res.data.email,
+                        message: res.data.message,
                     },
                 })
             })
@@ -38,49 +39,79 @@ class Contact extends React.Component{
             });
     }
 
-    handlenameChange= function(e) {
+    handlenameChange = function (e) {
         this.setState({
             contact: {
-                name : e.target.value,
-                email : this.state.contact.email,
-                message : this.state.contact.message,
+                name: e.target.value,
+                email: this.state.contact.email,
+                message: this.state.contact.message,
             }
         });
-     };
+    };
 
-     handleemailChange= function(e) {
+    handleemailChange = function (e) {
         this.setState({
             contact: {
-                name : this.state.contact.name,
-                email : e.target.value,
-                message : this.state.contact.message,
+                name: this.state.contact.name,
+                email: e.target.value,
+                message: this.state.contact.message,
             }
         });
-     };
+    };
 
-     handlemessageChange= function(e) {
+    handlemessageChange = function (e) {
         this.setState({
             contact: {
-                name : this.state.contact.name,
-                email : this.state.contact.email,
-                message : e.target.value,
+                name: this.state.contact.name,
+                email: this.state.contact.email,
+                message: e.target.value,
             }
         });
-     };
+    };
+
+    handleSubmit = (event) => {
+        if (this.state.contact.id == null) {
+            event.preventDefault();
+            axios
+                .post("/api/contact", this.state.contact, {
+                    headers: {
+                        apikey: "691c5597-e7d2-4c06-af49-f9369b367783",
+                    },
+                })
+                .then((response) => {
+                    alert(" Thêm liên hệ thành công ")
+                })
+                .catch((error) => alert(error));
+        }
+        else {
+            event.preventDefault();
+            axios
+                .put("/api/contact" + this.state.contact.id, this.state.contact, {
+                    headers: {
+                        apikey: "691c5597-e7d2-4c06-af49-f9369b367783",
+                    },
+                })
+                .then((response) => {
+                    alert(" update liên hệ thành công ")
+                })
+                .catch((error) => alert(error));
+        }
+    };
 
     render() {
+        console.log(this.state.contact);
         return (
             <div className="contact">
                 <div className="contact__infor">
                     <div className="contact__infor--me">
-                        <Information/>
+                        <Information />
                     </div>
                 </div>
                 <div className="contact__touch">
                     <div className="contact__touch--get">
                         <div className="contact__touch--get-information">
                             <h2>Get in touch</h2>
-                            <span> Nullam id color nibh ultriccies vehicula ut id elit . Morbi leo risus , porta ac , vestibulum at 
+                            <span> Nullam id color nibh ultriccies vehicula ut id elit . Morbi leo risus , porta ac , vestibulum at
                                 eros . NUlla vitae elit libero , a pharetra augue . Nulla vitae libero , a pharetra ague . Sed posuere
                                 consectetur est lobortis . Fusce dapibus , tellus ac cursus commde , tortor mauris condimentum nibh , ut
                                 fermentum massa justo sit amet risus.
@@ -93,9 +124,9 @@ class Contact extends React.Component{
                                 <label>Message</label>
                                 <textarea value={this.state.contact.message} placeholder="Enter your message" onChange={(event) => this.handlemessageChange(event)} ></textarea>
                             </form>
-                            <button>Submit</button>
+                            <button  onClick= {(event) => this.handleSubmit(event)} >Submit</button>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
